83000d84533ce8cbe31fb06a8690c59b3facee2c
nguyenduc97 <haiduc1120@gmail.com>
2021-03-04 09:58:10 +0700

    m

--- js/helper.js
+++ js/helper.js
@@ -11,16 +11,23 @@ require([
     "dojo/on",
     "dojo/dom",
     "esri/widgets/LayerList",
-    ],function(Map,MapView,Search,BasemapGallery,Expand,ScaleBar,HomeButton,Locate,query,dom,on,LayerList){
-    	var map = new Map({
-            basemap: "gray-vector"
-        });
-        var view = new MapView({
-            container: "viewDiv",
-            map: map,
-            center: [107.57, 16.475],
-            zoom: 13,
-        });
+    "esri/layers/MapImageLayer",
+    "esri/layers/GraphicsLayer",
+    "esri/layers/FeatureLayer",
+    ],function(Map,MapView,Search,BasemapGallery,Expand,ScaleBar,HomeButton,Locate,query,dom,on,LayerList,MapImageLayer,GraphicsLayer,FeatureLayer){
+       var basemapBDN = new MapImageLayer({
+        url: "http://103.77.167.158:6080/arcgis/rest/services/DTTM/ThuaDat/MapServer"
+    });
+       var map = new Map({
+        // basemap: "gray-vector"
+        layers: [basemapBDN] // bản đồ nền
+    });
+       var view = new MapView({
+        container: "viewDiv",
+        map: map,
+        center: [107.57, 16.475],
+        zoom: 13,
+    });
     // tìm kiếm
   //   var search = new Search({
   //     view: view
@@ -61,4 +68,42 @@ require([
     var layerList = new LayerList({
         view
     },"ddc_layerList");
+    // view info click
+     view.on("click", function(event) {
+        view.hitTest(event).then(function (response) {
+            console.log(response);
+        });
+        // view.hitTest(event).then(viewInfoPopup);
+    });
+     function viewInfoPopup(response) {
+        var titleField = response.results[0].graphic.layer;
+        var checkFieldInfo = response.results[0].graphic.geometry;
+        if (checkFieldInfo !== null) {
+            var graphics = response.results[0].graphic;
+            var fieldInfo = array.map(graphics.layer.fields, function(field) {
+                if (field.alias !== "OBJECTID") {
+                    return {
+                        "fieldName": field.name,
+                        "label": field.alias,
+                        "visible": true
+                    }
+                } else {
+                    return {
+                        "fieldName": field.name,
+                        "label": field.alias,
+                        "visible": false
+                    }
+                }
+            });
+            var template = {
+                title: titleField.title,
+                content: [{
+                    type: "fields",
+                    fieldInfos: fieldInfo
+                }],
+                action: "test"
+            };
+            graphics.layer.popupTemplate = template;
+        }
+    };
 });
